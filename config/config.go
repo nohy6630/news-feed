2c4699a8d60e0b94f8ded5beb3909d106836bd1f
nohy6630 <nohy6630@gmail.com>
2025-08-26 13:01:46 +0900

    config 수정

--- config/config.go
+++ config/config.go
@@ -8,7 +8,8 @@ import (
 )
 
 var (
-	infraAddress    string
+	kafkaAddress    string
+	redisAddress    string
 	mysqlAddress    string
 	infraConfigOnce sync.Once
 )
@@ -17,28 +18,39 @@ func loadInfraAddress() {
 	infraConfigOnce.Do(func() {
 		file, err := os.Open("config.json")
 		if err != nil {
-			infraAddress = "error"
+			kafkaAddress = "error"
+			redisAddress = "error"
+			mysqlAddress = "error"
 			return
 		}
 		defer file.Close()
 		var cfg struct {
-			InfraAddress string `json:"infra_address"`
+			KafkaAddress string `json:"kafka_address"`
+			RedisAddress string `json:"redis_address"`
 			MySQLAddress string `json:"mysql_address"`
 		}
 		if err := json.NewDecoder(file).Decode(&cfg); err != nil {
-			infraAddress = "error"
+			kafkaAddress = "error"
+			redisAddress = "error"
 			mysqlAddress = "error"
 		} else {
-			infraAddress = cfg.InfraAddress
+			kafkaAddress = cfg.KafkaAddress
+			redisAddress = cfg.RedisAddress
 			mysqlAddress = cfg.MySQLAddress
 		}
 	})
 }
 
-func GetInfraAddress() string {
+func GetKafkaAddress() string {
 	loadInfraAddress()
-	fmt.Printf("infraAddress: %s\n", infraAddress)
-	return infraAddress
+	fmt.Printf("kafkaAddress: %s\n", kafkaAddress)
+	return kafkaAddress
+}
+
+func GetRedisAddress() string {
+	loadInfraAddress()
+	fmt.Printf("redisAddress: %s\n", redisAddress)
+	return redisAddress
 }
 
 func GetMySQLAddress() string {
