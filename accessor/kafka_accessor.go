3ef1c4751968a6a6d22a4bddd1c344d560e5c877
nohy6630 <nohy6630@gmail.com>
2025-09-09 12:50:38 +0900

    fix kafka accessor

--- accessor/kafka_accessor.go
+++ accessor/kafka_accessor.go
@@ -6,6 +6,7 @@ import (
 	"github.com/twmb/franz-go/pkg/kgo"
 	"news-feed/config"
 	"sync"
+	"time"
 )
 
 type KafkaAccessor struct {
@@ -22,6 +23,13 @@ func NewKafkaAccessor(brokers []string, topic string) (*KafkaAccessor, error) {
 	opts = append(opts, kgo.SeedBrokers(brokers...))
 	opts = append(opts, kgo.ConsumerGroup("group"))
 	opts = append(opts, kgo.ConsumeTopics(topic))
+
+	// 프로듀서 ACK 설정
+	opts = append(opts, kgo.RequiredAcks(kgo.LeaderAck()))          // 모든 in-sync replica로부터 ACK 받기
+	opts = append(opts, kgo.RequestTimeoutOverhead(10*time.Second)) // 요청 타임아웃
+	opts = append(opts, kgo.ProduceRequestTimeout(15*time.Second))  // 프로듀스 타임아웃
+	opts = append(opts, kgo.RequestRetries(3))                      // 재시도 횟수
+
 	cl, err := kgo.NewClient(opts...)
 	if err != nil {
 		return nil, err
@@ -40,8 +48,10 @@ func GetKafkaAccessor() (*KafkaAccessor, error) {
 
 // Produce 함수 (동기)
 func (ka *KafkaAccessor) ProduceSync(ctx context.Context, topic string, value []byte) error {
+	fmt.Printf("produce sync topic=%s value=%s\n", topic, value)
 	record := &kgo.Record{Topic: topic, Value: value}
 	err := ka.Client.ProduceSync(ctx, record).FirstErr()
+	fmt.Printf("produce sync topic=%s value=%s\n", topic, value)
 	return err
 }
 
